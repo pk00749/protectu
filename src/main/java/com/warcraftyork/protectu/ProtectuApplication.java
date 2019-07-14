10c5f6c112b7006263d1010caa868f04c39baf5e
warcraftyork <pk00749@163.com>
2019-07-15 00:39:07 +0800

    init

---
+++ src/main/java/com/warcraftyork/protectu/ProtectuApplication.java
@@ -0,0 +1,13 @@
+package com.warcraftyork.protectu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectuApplication.class, args);
+	}
+
+}
