8467ac080b018e11d4166ceb75958122562377a5
warcraftyork <pk00749@163.com>
2019-08-01 01:07:33 +0800

    remove page under webapp

--- src/main/java/com/warcraftyork/ProtectuApplication.java
+++ src/main/java/com/warcraftyork/ProtectuApplication.java
@@ -1,4 +1,4 @@
-package com.warcraftyork.protectu;
+package com.warcraftyork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
