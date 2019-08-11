34b355c304bbc2498d393b2204aa4e2d9937f968
warcraftyork <pk00749@163.com>
2019-08-11 18:50:47 +0800

    change structure

--- src/main/java/com/warcraftyork/service/PoliciesService.java
+++ src/main/java/com/warcraftyork/service/PoliciesService.java
@@ -11,27 +11,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class PoliciesService {
 //    @Autowired
-//    private PoliciesMapper policiesMapper;
+    private PoliciesMapper policiesMapper;
 
 
     public String sayHello(String name){
         if(name == null){
             return "No Name to Select";
         }
-//        SqlSessionFactory factory = DbUtils.getSqlSessionFactory();
-//        SqlSession sqlSession = factory.openSession();
-//        PoliciesMapper testMapper = sqlSession.getMapper(PoliciesMapper.class);
-//        DBInfo info = testMapper.getDBInfo(name);
+        SqlSessionFactory factory = DbUtils.getSqlSessionFactory();
+        SqlSession sqlSession = factory.openSession();
+        PoliciesMapper policiesMapper = sqlSession.getMapper(PoliciesMapper.class);
+        DBInfo info = policiesMapper.getDBInfo(name);
 
-//        try {
-//            System.out.println(info.getHost() + " " + info.getDb() + " " + info.getUser());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            sqlSession.rollback();
-//        } finally {
-//            sqlSession.close();
-//        }
-//        return "Hello, " + info.getHost() + " " + info.getDb() + " " + info.getUser();
-        return name;
+        try {
+            System.out.println(info.getHost() + " " + info.getDb() + " " + info.getUser());
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            sqlSession.close();
+        }
+        return "Hello, " + info.getHost() + " " + info.getDb() + " " + info.getUser();
+//        return name;
     }
 }
