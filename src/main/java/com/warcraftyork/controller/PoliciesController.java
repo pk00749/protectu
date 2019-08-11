f33d09e5d534f24b2f0e8ab083169c36504d0fd9
warcraftyork <pk00749@163.com>
2019-08-11 14:39:02 +0800

    connect to db

--- src/main/java/com/warcraftyork/controller/PoliciesController.java
+++ src/main/java/com/warcraftyork/controller/PoliciesController.java
@@ -1,8 +1,14 @@
 package com.warcraftyork.controller;
 
+import com.warcraftyork.bean.DBInfo;
+import com.warcraftyork.mapper.PoliciesMapper;
+import com.warcraftyork.protectu.DbUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -12,6 +18,28 @@ public class PoliciesController {
     @RequestMapping("/test")
     public String test(){
         return "index";
+    }
+
+    @RequestMapping(value = "/sayHello")
+    @ResponseBody
+    public String sayHello(String name){
+        if(name == null){
+            return "No Name to Select";
+        }
+        SqlSessionFactory factory = DbUtils.getSqlSessionFactory();
+        SqlSession sqlSession = factory.openSession();
+        PoliciesMapper testMapper = sqlSession.getMapper(PoliciesMapper.class);
+        DBInfo info = testMapper.getDBInfo(name);
+
+        try {
+            System.out.println(info.getHost() + " " + info.getDb() + " " + info.getUser());
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            sqlSession.close();
+        }
+        return "Hello, " + info.getHost() + " " + info.getDb() + " " + info.getUser();
     }
 
     @GetMapping("/login")
